4bbe9b677ebe690f8b1900758e6b2d54b50e56fd
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2023-06-03 19:44:03 +0900

    TabNavigator add
    TabNavigator 추가해서 오른쪽으로 스와이프하면 화면 이동 되게 해줌

---
+++ Components/TabNavigator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+import Today from '../Pages/Today';
+import Tomorrow from '../Pages/Tomorrow';
+
+const Tab = createMaterialTopTabNavigator();
+
+function TabNavigator () {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false, // 라벨 숨김
+        showIcon: false, // 아이콘 숨김
+      }}
+    >
+      <Tab.Screen name="Today" component={Today}/>
+      <Tab.Screen name="Tomorrow" component={Tomorrow}/>
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
